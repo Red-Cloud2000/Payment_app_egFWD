46424cc12e5a25a8858c602a1b5b2800d5d01a29
Abdelrahman Hesham <61296209+Red-Cloud2000@users.noreply.github.com>
2022-10-03 09:16:21 +0200

    server.h

--- payment_app/Server/server.h
+++ payment_app/Server/server.h
@@ -1,20 +1,23 @@
-/*
- * server.h
+/******************************************************************************
  *
- *  Created on: Sep 16, 2022
- *      Author: abdoe
- */
+ * Module: Server
+ *
+ * File Name: server.h
+ *
+ * Description: Header file for the Server module
+ *
+ * Author: Abdelrahman Hesham
+ *
+ *******************************************************************************/
 
 #ifndef SERVER_H_
 #define SERVER_H_
 
-#include "../std_types.h"
-#include "../Card/card.h"
 #include "../Terminal/terminal.h"
 
-
-
-
+/*******************************************************************************
+ *                               Types Declaration                             *
+ *******************************************************************************/
 typedef enum EN_transState_t
 {
 	APPROVED, DECLINED_INSUFFECIENT_FUND, DECLINED_STOLEN_CARD, FRAUD_CARD, INTERNAL_SERVER_ERROR
@@ -53,16 +56,53 @@ typedef struct ST_accountsDB_t
 	uint8_t primaryAccountNumber[20];
 }ST_accountsDB_t;
 
+/*******************************************************************************
+ *                      Functions Prototypes                                   *
+ *******************************************************************************/
+
+/*
+ * Description : A function that helps the server to get all transaction data
+ *
+ */
 EN_transState_t recieveTransactionData(ST_transaction_t *transData);
+
+/*
+ * Description : A function to check if the card PAN is in the server Database or not
+ *
+ */
 EN_serverError_t isValidAccount(ST_cardData_t *cardData, ST_accountsDB_t *accountRefrence);
+
+/*
+ * Description : A function to check if the account is blocked or not
+ *
+ */
 EN_serverError_t isBlockedAccount(ST_accountsDB_t *accountRefrence);
+
+/*
+ * Description : A function to check if the transaction amount is available in
+ * 				 the client account or not
+ *
+ */
 EN_serverError_t isAmountAvailable(ST_terminalData_t *termData, ST_accountsDB_t *accountRefrence);
+
+/*
+ * Description : A function to check that everything is right to give a permission
+ * 				 of saving transaction or not
+ *
+ */
 EN_serverError_t saveTransaction(ST_transaction_t *transData);
+
+/*
+ * Description : A function to list all previous transactions details
+ *
+ */
 void listSavedTransactions(void);
 
+/*
+ * Description : A function to compare 2 strings 16 in length
+ *
+ */
 uint8_t my_strcmp(uint8_t *str1,uint8_t *str2);
-
-
 
 
 #endif /* SERVER_H_ */
