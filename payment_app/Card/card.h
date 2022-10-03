42101d6ae43562badfc98e15118e53f16f5879c2
Abdelrahman Hesham <61296209+Red-Cloud2000@users.noreply.github.com>
2022-10-03 09:14:32 +0200

    card.h

--- payment_app/Card/card.h
+++ payment_app/Card/card.h
@@ -1,37 +1,73 @@
-/*
- * card.h
+/******************************************************************************
  *
- *  Created on: Sep 16, 2022
- *      Author: abdoe
- */
+ * Module: Card
+ *
+ * File Name: card.h
+ *
+ * Description: Header file for the Card module
+ *
+ * Author: Abdelrahman Hesham
+ *
+ *******************************************************************************/
 
 #ifndef CARD_H_
 #define CARD_H_
 
 #include "../std_types.h"
 
+/***************************************************************************
+ *                    Definitions                                          *
+ ***************************************************************************/
 #define LESS         55
 #define MORE         65
+#define VALID        1
 
+#define TESTING_MODE 0
+
+/*******************************************************************************
+ *                               Types Declaration                             *
+ *******************************************************************************/
 typedef struct ST_cardData_t
 {
-uint8_t cardHolderName[25];
-uint8_t primaryAccountNumber[20];
-uint8_t cardExpirationDate[6];
+	uint8_t cardHolderName[25];
+	uint8_t primaryAccountNumber[50];
+	uint8_t cardExpirationDate[6];
 }ST_cardData_t;
+
 
 typedef enum EN_cardError_t
 {
-CARD_OK, WRONG_NAME, WRONG_EXP_DATE, WRONG_PAN
+	CARD_OK, WRONG_NAME, WRONG_EXP_DATE, WRONG_PAN
 }EN_cardError_t;
 
+
+/*******************************************************************************
+ *                      Functions Prototypes                                   *
+ *******************************************************************************/
+
+/*
+ * Description : A function to ask for the client card name
+ *
+ */
 EN_cardError_t getCardHolderName(ST_cardData_t *cardData);
+
+/*
+ * Description : A function to ask for the client card expiry date
+ *
+ */
 EN_cardError_t getCardExpiryDate(ST_cardData_t *cardData);
+
+/*
+ * Description : A function to ask for the client card PAN number
+ *
+ */
 EN_cardError_t getCardPAN(ST_cardData_t *cardData);
 
+/*
+ * Description : A function to check if the input length is right, Max or Min
+ *
+ */
 uint8_t Max_OR_Min(uint8_t len,uint8_t Max, uint8_t Min);
-
-uint8_t checkSum(uint8_t *pan);
 
 
 
